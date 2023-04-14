96c780a4c9c272b47cd787c0e3e4f96af3683e31
Yury Fomichev <fjurken@gmail.com>
2023-04-14 16:36:36 +0400

    project created

---
+++ src/main/java/com/jc_rent/jc_rent/JourneyCarRentApplication.java
@@ -0,0 +1,13 @@
+package com.jc_rent.jc_rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyCarRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyCarRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jc_rent/jc_rent/JourneyCarRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc_rent.jc_rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyCarRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
