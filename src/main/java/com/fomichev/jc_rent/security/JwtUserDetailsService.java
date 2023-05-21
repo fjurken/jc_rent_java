55a0af5b20122499d42b96d86e592a8087879e01
fjurken <fjurken@gmail.com>
2023-05-21 16:27:31 +0400

    first start

--- src/main/java/com/fomichev/jc_rent/security/JwtUserDetailsService.java
+++ src/main/java/com/fomichev/jc_rent/security/JwtUserDetailsService.java
@@ -1,6 +1,7 @@
 package com.fomichev.jc_rent.security;
 
 import com.fomichev.jc_rent.model.User;
+import com.fomichev.jc_rent.repository.UserRepository;
 import com.fomichev.jc_rent.security.jwt.JwtUser;
 import com.fomichev.jc_rent.security.jwt.JwtUserFactory;
 import com.fomichev.jc_rent.service.UserService;
@@ -15,16 +16,18 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class JwtUserDetailsService implements UserDetailsService {
 
-    private final UserService userService;
+    //    private final UserService userService;
+    private final UserRepository userRepository;
 
     @Autowired
-    public JwtUserDetailsService(UserService userService) {
-        this.userService = userService;
+    public JwtUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userService.findByUsername(username);
+//        User user = userService.findByUsername(username);
+        User user = userRepository.findByUsername(username);
         if (user == null) {
             throw new UsernameNotFoundException("User with username: " + username + " not found");
         }
